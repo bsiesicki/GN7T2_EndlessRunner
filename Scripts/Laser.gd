1625e5d5283244a7f7b5949484ddda27f7c3b8be
Bartek's Mac <bsiesicki@gmail.com>
2019-02-24 18:31:28 +0100

    procedural map generation stuff and failed attempts

--- Scripts/Laser.gd
+++ Scripts/Laser.gd
@@ -6,7 +6,7 @@ var rotation_speed = 1
 var platform_position_x = 80
 var platform_position_x2 = 240
 # pozycja lasera na osi Y
-var platform_position_y = 100
+var platform_position_y = 50
 
 #poruszanie się platformy
 var platform_speed = 20
@@ -15,10 +15,13 @@ var speed = Vector2()
 #kąt startowy do obrotu
 var angle = 1.570796
 
+const laser_scn = preload("res://Laser.tscn")
+
 onready var node2d
 onready var node2d2
 onready var node2dkb
 onready var node2d2kb
+var node2d3
 
 func _ready():
 	#wyciągnięcie nodów do zmiennych
@@ -26,6 +29,9 @@ func _ready():
 	node2d2 = get_node("Node2D2")
 	node2d2kb = get_node("Node2D2/KinematicBody2D")
 	node2dkb = get_node("Node2D/KinematicBody2D")
+	node2d3 = laser_scn.instance()
+	node2d3.position = Vector2(50.0,150)
+	add_child(node2d3)
 	#ustawienie poczatkowego konta obrotu
 	node2d.rotation = angle
 	pass
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -6,6 +6,11 @@ var dist_scale = 40
 onready var platform1
 onready var platform2
 onready var platform3
+var scale = Vector2(1.0,0.05)
+var rotation_speed = 0.01
+onready var camera
+
+const laser_scn = preload("res://Laser.tscn")
 
 func _ready():
 	character = get_node("Player")
@@ -13,9 +18,19 @@ func _ready():
 	platform1 = get_node("testLaser")
 	platform2 = get_node("StaticBody2D2")
 	platform3 = get_node("StaticBody2D3")
-	platform1.scale = Vector2(1.0,0.05)
+	camera = get_node("Camera2D")
+	platform1.scale = scale
+	platform2.scale = scale
+	platform3.scale = scale
 	pass
 
 func _physics_process(delta):
+	#Przemierzony dystans
 	indicator.text = str((int(character.position.x)/dist_scale)) + " m"
+#	for i in 100:
+#		var node2d = laser_scn.instance()
+#		if (int(character.position.x) == 200*i):
+#			node2d.scale = scale
+#			node2d.position = Vector2((character.position.x+150),100)
+#			add_child(node2d)
 	pass
