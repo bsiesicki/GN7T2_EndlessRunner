19ed87a3b5e9968c540ddeffb22a56a0e32e4928
BeardyBarber <kamajc87@gmail.com>
2019-03-03 14:48:24 +0100

    refactor Player.gd

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,20 +6,15 @@ export (int) var JUMP_SPEED = -350
 const JUMP_MAX_AIRBORNE_TIME = 0.2
 const FLOOR = Vector2(0, -1)
 
-enum State {RUNNING, SLIDING, JUMPING, FALLING}
+enum State {RUNNING, SLIDING, JUMPING, FALLING, DASHING, SKYDASHING}
 enum Swipe {LEFT, RIGHT, UP, DOWN, BLANK}
 
 
 var on_air_time = 100
-var swipeup = false
-var swipedown = false
-var swiperight = false
-var jumping = false
-var sliding = false
-var falling = false
-var dashing = false
-var running = true
-var onground = true
+
+export (State) var state = RUNNING
+export (Swipe) var swipe = BLANK
+
 var prev_jump_pressed = false
 var slide_start = 0
 var slide_end = 0
@@ -38,7 +33,7 @@ var elapsed_time = 0
 var start
 var speed
 var swipe_angle
-var start_time
+
 var direction = Vector2()
 export (float) var swipe_angle_const = 0.75
 var swipe_finished = false
@@ -47,201 +42,132 @@ export (int) var swipe_distance = 50
 func _ready():
 	$slideCollision.disabled = true
 	$dashCollision.disabled = true
-	$jumpCollision.disabled = true
-	$fallCollision.disabled = true
-	$runCollision.disabled = false
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	velocity.x = player_velocity
 	pass
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
 			start = event.position
-			start_time = elapsed_time
 		else:
 			direction = event.position - start
-			#speed = (direction.length())/(elapsed_time - start_time)
-			#direction = direction.normalized()
 			swipe_angle = direction.normalized().y
 			swipe_finished = true
 
 func _physics_process(delta):
-	velocity.x = player_velocity
 	elapsed_time += delta
-	if direction.y < -swipe_distance and swipe_finished == true and swipe_angle < -swipe_angle_const:
-		#velocity.y += JUMP_SPEED
-		swipeup = true
-		swipe_finished = false
-	if direction.y > swipe_distance and swipe_finished == true and swipe_angle > swipe_angle_const:
-		swipedown = true
-		#velocity.y -= JUMP_SPEED
-		swipe_finished = false
-	if direction.x > swipe_distance and swipe_finished == true and swipe_angle < swipe_angle_const/3:
-		swiperight = true
-		swipe_finished = false
-	velocity.y += GRAVITY  
+
 	velocity = move_and_slide(velocity, FLOOR)
 	
 	if (get_slide_count()>0):
 		var collider = get_slide_collision(0).collider
-		print("Collider: " + str(collider.name))
-		if (get_slide_collision(1) != null):
-			var collider2 = get_slide_collision(1).collider
-			print("Collider2: " + str(collider2.name))
-			if (collider2.is_class("KinematicBody2D")):
-				dead = true
 		if (collider.is_class("KinematicBody2D")):
 			dead = true
 
+	if direction.y < -swipe_distance and swipe_finished == true and swipe_angle < -swipe_angle_const:
+		swipe = UP
+		swipe_finished = false
 
+	elif direction.y > swipe_distance and swipe_finished == true and swipe_angle > swipe_angle_const:
+		swipe = DOWN
+		swipe_finished = false
 
-	if (swipeup and onground and !sliding):
-		velocity.y += JUMP_SPEED
-		jumping = true
-		swipeup = false
-		$slideCollision.disabled = true
-		$runCollision.disabled = true
-		$dashCollision.disabled = true
-		$jumpCollision.disabled = false
-		$fallCollision.disabled = true
+	elif direction.x > swipe_distance and swipe_finished == true and swipe_angle < swipe_angle_const/3:
+		swipe = RIGHT
+		swipe_finished = false
+#	if(swipe != BLANK):
+#		print(swipe)
 		
-	if (swipedown and onground):
-		$slideCollision.disabled = false
-		$runCollision.disabled = true
-		$dashCollision.disabled = true
-		$jumpCollision.disabled = true
-		$fallCollision.disabled = true
-		sliding = true
-		onground = true
-		falling = false
-		jumping = false
-		running = false
-		swipedown = false
-		slide_start = self.position.x
-	if (swipedown and !onground):
-		velocity.y -= JUMP_SPEED*2
-		$slideCollision.disabled = true
-		$runCollision.disabled = true
-		$dashCollision.disabled = true
-		$jumpCollision.disabled = true
-		$fallCollision.disabled = false
-		falling = true
-		swipedown = false
-	if (swipeup and onground and sliding):
-		$runCollision.disabled = true
-		$dashCollision.disabled = true
-		$slideCollision.disabled = true
-		$fallCollision.disabled = true
-		$jumpCollision.disabled = false
-		jumping = false
-		sliding = false
-	if (swiperight and onground and !sliding and !jumping and !falling):
-		$dashCollision.disabled = false
-		$runCollision.disabled = true
-		$slideCollision.disabled = true
-		$fallCollision.disabled = true
-		$jumpCollision.disabled = true
-		dash_start = self.position.x
-		running = false
-		dashing = true
-		swiperight = false
-	if (swiperight and (jumping or falling) and canDash):
-		$dashCollision.disabled = false
-		$runCollision.disabled = true
-		$slideCollision.disabled = true
-		$jumpCollision.disabled = true
-		$fallCollision.disabled = true
-		dash_start = self.position.x
-		temp_y_velocity = velocity.y
-		if (jumping):
-			temp_state = jumping
-		else:
-			temp_state = falling
-		jumping = false
-		falling = false
-		dashing = true
-		canDash = false
-		swiperight = false
-	if (swiperight and sliding):
-		$runCollision.disabled = true
-		$slideCollision.disabled = true
-		$dashCollision.disabled = false
-		$fallCollision.disabled = true
-		$jumpCollision.disabled = true
-		dash_start = self.position.x
-		sliding = false
-		dashing = true
-		swiperight = false
-		sliding = false
+	if (swipe == DOWN):
+		if (state == RUNNING):
+			state = SLIDING
+			slide_start = self.position.x
 		
-	if (sliding):
-		slide_end = (self.position.x)
+		elif (state == JUMPING or state == SKYDASHING):
+			velocity.y -= JUMP_SPEED*2
+			velocity.x = player_velocity
+			state = FALLING
+		swipe = BLANK
+
+	if (swipe == UP):
+		if (state == RUNNING or state == SLIDING):
+			velocity.y += JUMP_SPEED
+			state = JUMPING
+		swipe = BLANK
+		
+	if (swipe == RIGHT):
+		if(canDash):
+			dash_start = self.position.x
+			if(state == JUMPING or state == FALLING):
+				state = SKYDASHING
+				canDash = false
+			else:
+				state = DASHING
+		swipe = BLANK
+		
+	if (state == SLIDING):
+		slide_end = self.position.x
 		if (slide_end - slide_start > slide_distance):
-			$runCollision.disabled = false
-			$dashCollision.disabled = true
-			$slideCollision.disabled = true
-			$jumpCollision.disabled = true
-			$fallCollision.disabled = true
-			sliding = false
+
+			state = RUNNING
 			
-	if (dashing):
+	elif (state == DASHING or state == SKYDASHING):
 		dash_end = self.position.x
-		player_velocity = dash_velocity
+		velocity.x = dash_velocity
 		velocity.y = 0
 		if (dash_end - dash_start > dash_distance):
-			$runCollision.disabled = false
-			$dashCollision.disabled = true
-			$slideCollision.disabled = true
-			$jumpCollision.disabled = true
-			$fallCollision.disabled = true
-			player_velocity = 150
-			velocity.y = temp_y_velocity
-			dashing = false
-			if (temp_state == falling):
-				falling = true
-			elif (temp_state == jumping):
-				jumping = true
-	
-	if (velocity.y < 0 and !onground):
-		jumping = true
-		sliding = false
-		running = false
-		$runCollision.disabled = true
-		$dashCollision.disabled = true
-		$slideCollision.disabled = true
-		$fallCollision.disabled = true
-		$jumpCollision.disabled = false
-	elif (velocity.y > 0 and !onground):
-		falling = true
-		running = false
-		jumping = false
-		$runCollision.disabled = true
-		$dashCollision.disabled = true
-		$slideCollision.disabled = true
-		$fallCollision.disabled = false
-		$jumpCollision.disabled = true
-	else:
-		falling = false
-		jumping = false
-		
-	if (!jumping and !falling and !sliding and !dashing):
-		running = true
-		
-	if (is_on_floor()):
-		onground = true
-		canDash = true
-	else:
-		onground = false
-	
-	if (jumping):
-		$PlayerSprite.play("ninjaJump")
-	elif (falling):
-		$PlayerSprite.play("ninjaFall")
-	elif (running):
-		$PlayerSprite.play("ninjaRun")
-	elif (sliding):
-		$PlayerSprite.play("ninjaSlide")
-	elif (dashing):
-		$PlayerSprite.play("ninjaDash")
+			velocity.x = player_velocity
+			velocity.y = 0
+			if (state == DASHING):
+				state = RUNNING
+			else:
+				state = FALLING
 
+	if velocity.y > 0:
+		state = FALLING
+
+	if (is_on_floor()):
+		canDash = true
+		if(state == FALLING):
+			velocity.y = 0
+			velocity.x = player_velocity
+			state = RUNNING
+	else:
+		velocity.y += GRAVITY 
+		
+	if (state == JUMPING):
+		$PlayerSprite.play("ninjaJump")
+		$slideCollision.disabled = false
+		$dashCollision.disabled = false
+		$jumpCollision.disabled = true
+		$fallCollision.disabled = false
+		$runCollision.disabled = false
+	elif (state == FALLING):
+		$PlayerSprite.play("ninjaFall")
+		$slideCollision.disabled = false
+		$dashCollision.disabled = false
+		$jumpCollision.disabled = false
+		$fallCollision.disabled = true
+		$runCollision.disabled = false
+	elif (state == RUNNING):
+		$PlayerSprite.play("ninjaRun")
+		$slideCollision.disabled = false
+		$dashCollision.disabled = false
+		$jumpCollision.disabled = false
+		$fallCollision.disabled = false
+		$runCollision.disabled = true
+	elif (state == SLIDING):
+		$PlayerSprite.play("ninjaSlide")
+		$slideCollision.disabled = true
+		$dashCollision.disabled = false
+		$jumpCollision.disabled = false
+		$fallCollision.disabled = false
+		$runCollision.disabled = false
+	elif (state == DASHING):
+		$PlayerSprite.play("ninjaDash")
+		$slideCollision.disabled = false
+		$dashCollision.disabled = true
+		$jumpCollision.disabled = false
+		$fallCollision.disabled = false
+		$runCollision.disabled = false
+		
