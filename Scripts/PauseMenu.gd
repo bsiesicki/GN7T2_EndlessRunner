b1768cb0c90e843d1640942098903cb491ea91e7
Bartek's Mac <bsiesicki@gmail.com>
2019-03-04 17:55:30 +0100

    Mini audio fix

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -4,7 +4,6 @@ var position
 
 func _on_TextureButton_pressed():
 	get_tree().paused = true
-	position = get_parent().get_node("Camera2D/bgm").get_playback_position()
 	get_parent().get_node("Camera2D/bgm").volume_db = -40
 	#get_parent().get_node("AudioStreamPlayer2D").stop()
 	$pausePopup.show()
@@ -12,7 +11,6 @@ func _on_TextureButton_pressed():
 func _on_resumeButton_pressed():
 	$pausePopup.hide()
 	if (get_node("/root/global").music == true):
-		get_parent().get_node("Camera2D/bgm").play(position)
 		get_parent().get_node("Camera2D/bgm").volume_db = -30
 	get_tree().paused = false
 
