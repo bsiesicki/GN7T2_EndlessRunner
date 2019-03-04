277ba6574dca4c130607278198a2a3a2d920a3f2
Bartek's Mac <bsiesicki@gmail.com>
2019-03-04 17:45:27 +0100

    Pause BGM fix

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -4,16 +4,16 @@ var position
 
 func _on_TextureButton_pressed():
 	get_tree().paused = true
-	position = get_parent().get_node("Camera2D/AudioStreamPlayer2D").get_playback_position()
-	get_parent().get_node("Camera2D/AudioStreamPlayer2D").volume_db = -40
+	position = get_parent().get_node("Camera2D/bgm").get_playback_position()
+	get_parent().get_node("Camera2D/bgm").volume_db = -40
 	#get_parent().get_node("AudioStreamPlayer2D").stop()
 	$pausePopup.show()
 
 func _on_resumeButton_pressed():
 	$pausePopup.hide()
 	if (get_node("/root/global").music == true):
-		get_parent().get_node("Camera2D/AudioStreamPlayer2D").play(position)
-		get_parent().get_node("Camera2D/AudioStreamPlayer2D").volume_db = -30
+		get_parent().get_node("Camera2D/bgm").play(position)
+		get_parent().get_node("Camera2D/bgm").volume_db = -30
 	get_tree().paused = false
 
 
